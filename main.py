7cec06553e6894051909c5bd27c430efff15c0cb
Lenovo <avg090909@gmail.com>
2023-11-03 22:14:55 +0200

    кг

--- main.py
+++ main.py
@@ -39,49 +39,148 @@
 # print(f"тварин у зоопарку: {a.total_a}")
 
 
-class Car:
-    brand = "Без назви"
+# class Car:
+#   brand = "Без назви"
 
-    def __init__(self, color, year, speed=0, c=False, lights=False, ):
-        self.color = color
-        self.year = year
-        self.speed = speed
-        self.c = c
-        self.lights = lights
+#  def __init__(self, color, year, speed=0, c=False, lights=False, ):
+# self.color = color
+# self.year = year
+# self.speed = speed
+# self.c = c
+# self.lights = lights
 
-    def onlights(self):
-        self.lights = True
-        print(f"ого я щось бачу")
+# def onlights(self):
+#   self.lights = True
+#  print(f"ого я щось бачу")
 
-    def offlights(self):
-        self.lights = False
-        print(f" я нічого не бачу")
+# def offlights(self):
+#   self.lights = False
+#  print(f" я нічого не бачу")
 
-    def onc(self):
-        self.c = True
-        print(f"холодно")
+# def onc(self):
+#   self.c = True
+#  print(f"холодно")
 
-    def offc(self):
-        self.c = False
-        print(f"душно")
+# def offc(self):
+#   self.c = False
+#  print(f"душно")
 
-    def ride(self):
-        if self.speed == 0:
-            print(f"{self.color} автомобіль марки {self.brand} року {self.year} зупинився")
+# def ride(self):
+#   if self.speed == 0:
+#      print(f"{self.color} автомобіль марки {self.brand} року {self.year} зупинився")
+# else:
+#    print(f"{self.color} автомобіль марки {self.brand} року {self.year} їде")
+
+
+# car1 = Car("Cиній", 2009, c=True, lights=False)
+# car1.brand = "Volvo"
+
+# car2 = Car("Сшрий", 2011, lights=True, c=False)
+# car2.brand = "Honda"
+
+# car1.onlights()
+# car1.offc()
+# car1.ride(0)
+
+# car2.offlights()
+# car2.onc()
+# car2.ride(23)
+
+
+import random
+
+import self
+
+
+class Human:
+    def __init__(self, name="Human", job=None, home=None, car=None):
+        self.name = name
+        self.money = 100
+        self.gladness = 50
+        self.satiety = 50
+        self.job = job
+        self.home = home
+        self.car = car
+
+    def home(self):
+        self.home = House
+
+    def car(self):
+        self.car = Auto(brands)
+
+    def job(self):
+        if self.car.drive():
+            pass
         else:
-            print(f"{self.color} автомобіль марки {self.brand} року {self.year} їде")
+            self.to_repair()
+            return
+        self.job = self.job(job_list)
 
 
-car1 = Car("Cиній", 2009, c=True, lights=False)
-car1.brand = "Volvo"
+h = Human('Alex', True)
+print(h.job)
 
-car2 = Car("Сшрий", 2011, lights=True, c=False)
-car2.brand = "Honda"
 
-car1.onlights()
-car1.offc()
-car1.ride(0)
+class House:
+    def __init__(self):
+        mess = 0
+        food = 0
 
-car2.offlights()
-car2.onc()
-car2.ride(23)
+
+class Auto:
+    def __init__(self, brand_list):
+        self.brand = random.choice(list(brand_list))
+        self.fuel = brand_list[self.brand]["fuel"]
+        self.strength = brand_list[self.brand]["strength"]
+        self.consumption = brand_list[self.brand]["consumption"]
+
+    def drive(self):
+        if self.strength > 0 and self.fuel >= self.consumption:
+            self.fuel -= self.consumption
+            self.strength -= 1
+            return True
+        else:
+            print("The car cannot move!")
+            return False
+
+
+class Job:
+    def __init__(self, job_list):
+        self.job = random.choice(list(job_list))
+        self.salary = job_list[self.job]['salary']
+        self.gladness_less = job_list[self.job]['gladness_less']
+
+
+class Sims:
+    def __init__(self, mood, clutter, money, car_condition):
+        self.mood = mood
+        self.clutter = clutter
+        self.money = money
+        self.car_condition = car_condition
+
+    def chill(self):
+        self.mood += 10
+        self.clutter += 5
+
+    def clean_home(self):
+        self.mood -= 5
+        self.clutter = 0
+
+    def to_repair(self):
+        self.car_condition += 100
+        self.money -= 50
+
+
+job_list = {
+    "Java developer": {"salary": 50, "gladness_less": 10},
+    "Python developer": {"salary": 40, "gladness_less": 3},
+    "C++ developer": {"salary": 60, "gladness_less": 25},
+    "Rust developer": {"salary": 70, "gladness_less": 15},
+}
+
+brands = {
+    "BMW": {"fuel": 100, "strength": 100, "consumption": 6},
+    "Lada": {"fuel": 50, "strength": 40, "consumption": 10},
+    "Volvo": {"fuel": 80, "strength": 150, "consumption": 8},
+    "Ferrari": {"fuel": 80, "strength": 120, "consumption": 14}
+}
